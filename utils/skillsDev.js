ed1bf5ac0a1303d0fc02bc00add414e22c67ad61
Daniel Salvatierra Sanchez <dss250583@gmail.com>
2024-07-01 15:41:31 +0200

    Add files via upload

---
+++ utils/skillsDev.js
@@ -0,0 +1,29 @@
+export const skillsUsed = [
+  { title: 'HTML5', img: './html.webp' },
+  { title: 'CCS3', img: './css.webp' },
+  { title: 'JavaScript', img: './js.webp' },
+  { title: 'Vite', img: './vite.webp' },
+  { title: 'VS Code', img: './vscode.webp' },
+  { title: 'Github', img: './github.webp' },
+  { title: 'Vercel', img: './vercel.webp' },
+  { title: 'Netlify', img: './netlify.webp' },
+  { title: 'Responsive', img: './responsive2.webp' },
+  { title: 'Git', img: './git.webp' }
+];
+
+export const skillsLearning = [
+  { title: 'Node.js', img: './nodejs.webp' },
+  { title: 'MongoDB', img: './mongodb.webp' },
+  { title: 'API REST', img: './api.webp' },
+  { title: 'React', img: './react.webp' },
+  { title: 'Angular', img: './angular.webp' },
+  { title: 'Next.js', img: './nextjs.webp' },
+  { title: 'Figma', img: './figma.webp' },
+  { title: 'TypeScript', img: './typescript.webp' },
+  // { title: 'Python', img: './python.webp' },
+];
+
+export const skillsOther = [
+  { title: 'Spanish', img: './es.webp', text: 'Native Language' }, 
+  { title: 'English', img: './uk.webp', text: 'B1 / B2' },
+];
